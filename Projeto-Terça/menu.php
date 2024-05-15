8a05c3aa623686f5859987630e2940bcec853f12
Richard Gomes de Oliveira <richard1_peterson@hotmail.com>
2024-05-15 22:56:22 +0000

    inclusão de estrutura de produtos

---
+++ Projeto-Terça/menu.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>janelas-itens</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="card-container">
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <form>
+            <input type="radio" id="opcao1" name="opcao" value="opcao1">
+            <label for="opcao1"></label>
+          
+            <input type="radio" id="opcao2" name="opcao" value="opcao2">
+            <label for="opcao2"></label>
+          
+            <input type="radio" id="opcao3" name="opcao" value="opcao3">
+            <label for="opcao3"></label>
+          </form>
+          
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+<div class="card">
+    <img src="parte de tras carta.jpg">
+    <div>
+        <h1>camisa brasil</h1>
+        <h2>Descrição</h2>
+        <span>R$ 5,50</span>
+        <button> saiba mais</button>
+
+    </div>
+</div>
+
+</div>
+
+</body>
+</html>
